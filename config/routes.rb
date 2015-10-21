77448377f6e85cd3ab1c11d3df009b5cf61b8303
Yuri <yuri@simplee.com>
2015-10-21 16:29:50 +0300

    CRUD assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
+  resources :posts
 
-  get 'welcome/contact'
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
